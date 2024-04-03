69bc123975bc753480ba45a0034061446b2567cd
jcheetham3906 <128601268+jcheetham3906@users.noreply.github.com>
2024-04-03 07:26:13 +0700

    Made first classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,23 @@
+// Jimi
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
+
